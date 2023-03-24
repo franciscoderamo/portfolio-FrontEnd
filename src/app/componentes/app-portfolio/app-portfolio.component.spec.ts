ce3b7596faefdb8c82db5fb122b5da29d901ee01
franciscoderamo <franciscoderamo@gmail.com>
2023-03-24 01:40:25 -0300

    feature: create component app-portfolio

---
+++ src/app/componentes/app-portfolio/app-portfolio.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AppPortfolioComponent } from './app-portfolio.component';
+
+describe('AppPortfolioComponent', () => {
+  let component: AppPortfolioComponent;
+  let fixture: ComponentFixture<AppPortfolioComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AppPortfolioComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(AppPortfolioComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
