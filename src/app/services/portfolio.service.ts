5cbef164aedcf9e95f3d9cfdd2a5bea9d19c064c
franciscoderamo <franciscoderamo@gmail.com>
2023-03-26 22:31:19 -0300

    feature: add services model

---
+++ src/app/services/portfolio.service.ts
@@ -0,0 +1,38 @@
+import { HttpClient } from '@angular/common/http';
+import { identifierName } from '@angular/compiler';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+//import { environment } from 'src/environments/environment';
+import { Portfolio } from '../model/portfolio';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PortfolioService {
+
+  URL = 'http://localhost:8080/portfolio/'; // 'https://portfolio-francisco-deramo.onrender.com/portfolio';
+
+  //URL = environment.URL + 'portfolio/';
+
+  constructor(private httpClient: HttpClient) { }
+
+  public all(): Observable<Portfolio[]>{
+    return this.httpClient.get<Portfolio[]>(this.URL + 'all');
+  }
+
+  public see(id: number): Observable<Portfolio>{
+    return this.httpClient.get<Portfolio>(this.URL + `see/${id}`);
+  }
+
+  public new(portfolio: Portfolio): Observable<any>{
+    return this.httpClient.post<any>(this.URL + 'new', portfolio);
+  }
+
+  public edit(id: number, portfolio: Portfolio): Observable<any>{
+    return this.httpClient.put<any>(this.URL + `edit/${id}`, portfolio);
+  }
+
+  public delete(id: number): Observable<any>{
+    return this.httpClient.delete<any>(this.URL + `delete/${id}`);
+  }
+}
