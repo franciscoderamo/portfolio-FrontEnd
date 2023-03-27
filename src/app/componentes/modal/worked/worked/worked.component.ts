3eb5dd3bdc4cb7e11be89b26fdc42ebcbdaf2592
franciscoderamo <franciscoderamo@gmail.com>
2023-03-26 23:01:57 -0300

    feature: add components modal

---
+++ src/app/componentes/modal/worked/worked/worked.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-worked',
+  templateUrl: './worked.component.html',
+  styleUrls: ['./worked.component.css']
+})
+export class WorkedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
